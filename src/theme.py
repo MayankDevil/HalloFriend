c9b9676a4b477a3e96f4d1a9a6880c69f7c2c486
मयंक <mastermayank91@gmail.com>
2024-05-04 15:04:45 +0000

    Add src/theme

---
+++ src/theme.py
@@ -0,0 +1,19 @@
+
+class Theme:
+
+    @staticmethod
+    def welcome(text):
+        print("\n\n",
+              "\n   WW        WW  ",
+              "\n   WW        WW  ",
+              "\n   WW        WW  EE EE EE  LL        CC CC CC  OO OO OO  MMM    MMM  EE EE EE ",
+              "\n   WW        WW  EE        LL        CC        OO    OO  MM MMMM MM  EE       ",
+              "\n   WW        WW  EE        LL        CC        OO    OO  MM  MM  MM  EE       ",
+              "\n   WW   WW   WW  EE EE     LL        CC        OO    OO  MM  MM  MM  EE EE    ",
+              "\n   WW   WW   WW  EE        LL        CC        OO    OO  MM      MM  EE       ",
+              "\n   WW WW  WW WW  EE        LL        CC        OO    OO  MM      MM  EE       ",
+              "\n   WWWW    WWWW  EE EE EE  LL LL LL  CC CC CC  OO OO OO  MM      MM  EE EE EE ",
+              "\n\n",
+              "-"*80,
+              f"\n\n   [ {text.upper()} ]\n\n")
+
